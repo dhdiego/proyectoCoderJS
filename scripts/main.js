9bf793a08cee116364b0afea986840e690c3dab0
Diego Diaz <diaz.dh@gmail.com>
2023-01-09 21:03:39 -0300

    se agregaron productos a seccion

--- scripts/main.js
+++ scripts/main.js
@@ -1,117 +1,114 @@
+const productos = [
+    // vinilos
+    {
+        id: 'vinilo-01',
+        titulo: 'Vinilo: The Doors - Strange Days',
+        imagen: './img/vinilos/The Doors – Strange Days.jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 13500,
+    },
 
-/// ENCICLOPEDIA MUNDIALISTICA
+    {
+        id: 'vinilo-02',
+        titulo: 'Vinilo: The Babasonicos - Infame',
+        imagen: './img/vinilos/Babasonicos – Infame.jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 9900,
+    },
 
-// PUEDE INGRESAR LOS DATOS DE UN JUGAROS, DARLO DE BAJA, MODIFICARLO Y CONSULTAR SU FICHA TECNICA INGRESANDO EL NUMERO DE PUESTO DEL JUGADOR.
+    {
+        id: 'vinilo-03',
+        titulo: 'Vinilo: Iron Maiden - The Number of the beast',
+        imagen: './img/vinilos/Iron Maiden – The Number Of The Beast.jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 10500,
+    },
 
-//MESSI = 10
-// De Paul = 7
-// DiMaria = 23
-//dibu = 1
-//Alvarez = 19 
+    {
+        id: 'vinilo-04',
+        titulo: 'Vinilo: The Rolling Stones - Tatto You',
+        imagen: './img/vinilos/Rolling Stones – Tattoo You.jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 13500,
+    },
+
+    {
+        id: 'vinilo-05',
+        titulo: 'Vinilo: Thelonious Monk – Genius Of Modern Music',
+        imagen: './img/vinilos/Thelonious Monk – Genius Of Modern Music (Volume One).jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 16800,
+    },
+    {
+        id: 'vinilo-06',
+        titulo: 'Vinilo: Led Zeppelin - Led Zeppelin I',
+        imagen: './img/vinilos/Led Zeppelin – Led Zeppelin.jpg',
+        categoria: {
+            nombre: 'vinilos',
+            id: 'vinilo'
+        },
+        precio: 14000,
+    },
+
+    
 
 
-class Jugador {
-    constructor(nombre, apellido, puesto, calificacion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.puesto = puesto;
-        this.calificacion = calificacion;
-    }
+];
+
+const contenedorProductos = document.querySelector('#contenedor-productos');
+
+function cargarProductos(productosElegidos) {
+
+  
+
+    productos.forEach(producto => {
+        const div = document.createElement('div');
+
+        div.classList.add('producto');
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+        <div class="producto-detalles">
+            <h3 class="producto-titulo">${producto.titulo} </h3>
+            <p class="producto-precio">Precio: $${producto.precio}</p>
+            <button class="producto-agregar" id="${producto.id}">Agregar</button>
+        </div>`;
+
+        contenedorProductos.append(div);
+
+    })
+
 }
 
-const jugadorMessi = new Jugador('Lionel', 'Messi', 10, 10 );
-const jugadorDePaul = new Jugador('Rodrigo', 'De Paul', 7, 7 );
-const jugadorDiMaria = new Jugador('Angel', 'Di Maria', 23, 7 );
-const jugadorDibu = new Jugador('Damian', 'Martinez', 1 , 9 );
-const jugadorAlvarez = new Jugador('Julian', 'Alvarez', 19 , 5 );
-
-const arrayJugador = [];
-
-arrayJugador.push(jugadorMessi);
-arrayJugador.push(jugadorDePaul);
-arrayJugador.push(jugadorDiMaria);
-arrayJugador.push(jugadorDibu);
-arrayJugador.push(jugadorAlvarez);
-console.log(arrayJugador)
-
-function menu() {
-    alert('Bienvenido a la Selección');
-    let opcion = parseInt(prompt('Ingrese una opcion: \n 1) Alta de Jugador, \n2)Baja de Jugador, \n3) Modificación de Jugador, \n4) Consultar Ficha Tecnica, \n5) Salir'));
-    return opcion;
-}
+cargarProductos();
 
 
+/* const productosArray = [
+    {
+    id: 'vinilo-01',
+    titulo: 'Vinilo: The Doors - Strange Days',
+    imagen: './img/vinilos/The Doors – Strange Days.jpg',
+    categoria: {
+        nombre: 'vinilos',
+        id: 'vinilo'
+    },
+    precio: 13500,
 
-// Dar de alta un jugador
-
-function altaJugador(){
-    let nombre = prompt('Ingrese el nombre del Jugador: ');
-    let apellido = prompt('Ingrese el apellido del Jugador: ');
-    let puesto = parseInt(prompt('Ingrese el Puesto del jugador'));
-    let calificacion = parseInt(prompt('Ingrese la calificación del jugador'));
-    let jugador = new Jugador(nombre, apellido, puesto, calificacion);
-    arrayJugador.push(jugador);
-    console.log(arrayJugador);
-}
-
-// dar de baja un jugador (ingresar el puesto por Ejemplo Messi = 10)
-
-function bajaJugador(){
-    let puesto = parseInt(prompt('Ingrese el Puesto del jugador: \n A) MESSI = 10, \n B) De Paul = 7,  \n C) DiMaria = 23, \n D) Dibu Martinez = 1, \nE)Julian Alvarez = 19 '));
-    let jugador = arrayJugador.find(jugador => jugador.puesto === puesto);
-    let indice = arrayJugador.indexOf(jugador);
-    arrayJugador.splice(indice, 1);
-    console.log(arrayJugador);
-}
-
-// modificar un jugador (ingresar el puesto ejemplo messi = 10)
-
-function modificarJugador(){
-    let puesto = parseInt(prompt('Ingrese el Puesto del jugador: \n A) MESSI = 10, \n B) De Paul = 7,  \n C) DiMaria = 23, \n D) Dibu Martinez = 1, \nE)Julian Alvarez = 19 '));
-    let jugador = arrayJugador.find(jugador => jugador.puesto === puesto);
-    let indice = arrayJugador.indexOf(jugador);
-    let nombre = prompt('Ingrese el nombre del Jugador');
-    let apellido = prompt('Ingrese el apellido del jugador');
-    let calificacion = parseInt(prompt('Ingrese la Calificacion del jugador'));
-    let jugadorModificado = new Jugador (nombre, apellido, puesto, calificacion);
-    arrayJugador.splice(indice, 1, jugadorModificado);
-    console.log(arrayJugador);
 
 }
+] */
 
-// consultar un jugador (debe ingresar el numero de camiseta ejemplo Messi =10)
-
-function consultaJugador(){
-    let puesto = parseInt(prompt('Ingrese el Puesto del jugador: \n A) MESSI = 10, \n B) De Paul = 7,  \n C) DiMaria = 23, \n D) Dibu Martinez = 1, \nE)Julian Alvarez = 19 '));
-    let jugador = arrayJugador.find(jugador => jugador.puesto === puesto);
-    console.log(jugador);
-}
-
-// salir 
-
-function salir() {
-    alert('Gracias por ver nuestro Visualizador Mundialistico');
-}
-
-let opcion = menu();
-switch (opcion) {
-    case 1:
-        altaJugador();
-        break;
-    case 2:
-        bajaJugador();
-        break;
-    case 3: 
-        modificarJugador();
-        break;
-    case 4: 
-        consultaJugador();
-        break;
-    case 5: 
-        salir();
-        break;
-    default:
-        alert('No ha ingresado una opcion valida');
-        break;
-        
-}
